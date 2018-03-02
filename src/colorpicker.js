12870c4bf7e6f1838cb48466563defcaeec95bef
sangle7 <sanglewang@gmail.com>
2018-03-02 17:51:33 +0800

    add config

--- src/colorpicker.js
+++ src/colorpicker.js
@@ -1,22 +1,28 @@
 
 import React from 'react'
-import { calculateColor, generateColor, gPositionByColor } from './utils'
+import { calculateColor, generateColor,gTarget, gPositionByColor, calculatePositionOnLine } from './utils'
 
 
 const ColorInput = (props) => {
-    const { onChange,labelText,v,step } = props
+    const { onChange,labelText,v,step,type = 'number'  } = props
     return (
-        <label className='colorpickerLabel'>{labelText}:
-            <input value={v} onChange={onChange} step={step} className='testTxt green' type='number'/>
-        </label>
+        <input value={v} onChange={onChange} step={step} className='testTxt green' type={type} />
     )
 }
 
 export class Colorpicker extends React.Component{
     constructor(props) {
         super(props)
+        /*设置：排版（横/竖），样式（c1宽，高），开启input（hsl，rgb，hex）,是否可见*/
+        this.config = {
+            mode:"horizontal",
+            hsl:true,   //render
+            rgb:true,   //render
+            hex:true,   //render
+            visible: true,  //render
+            ...this.props
+        }
         this.state = {
-            visible: props.visible,
             color:{
                 R:255,
                 G:0,
@@ -31,14 +37,14 @@ export class Colorpicker extends React.Component{
                 left:'391px'
             },
             position2:{
-                top:'-9px'
+                top:'-9px',
             },
             bgColor:'rgb(255,0,0)'
         }
     }
 
-    initCanvas = () => {
-        const gar2 = this.context2.createLinearGradient(0, 0, 0, 400);
+    initCanvas = (w,h) => {
+        const gar2 = this.context2.createLinearGradient(0, 0, 0, h);
         gar2.addColorStop(0, 'rgb(255,0,0)');
         gar2.addColorStop(0.166, 'rgb(255,255,0)');
         gar2.addColorStop(0.33, 'rgb(0,255,0)');
@@ -47,20 +53,21 @@ export class Colorpicker extends React.Component{
         gar2.addColorStop(0.837, 'rgb(255,0,255)');
         gar2.addColorStop(1, 'rgb(255,0,0)');
         this.context2.fillStyle = gar2;
-        this.context2.fillRect(0, 0, 20, 400);
+        this.context2.fillRect(0, 0, w,h);
     }
 
 
     componentDidMount(){
+        console.log(this.config)
         this.context2 = this.canvasline.getContext('2d');
-        this.canvasline.width = 20;
+        this.canvasline.width = 10;
         this.canvasline.height = 400;
-        this.initCanvas();
+        this.initCanvas(this.canvasline.width,this.canvasline.height);
         /*生成默认的小球位置 */
         this.canvas.addEventListener('mousedown',e=>{
             this.isDragCanvas = true
         })
-        this.canvasline.addEventListener('mousedown',e=>{
+        this.canvaslinecontainer.addEventListener('mousedown',e=>{
             this.isDragCanvasLine = true
         })
         document.addEventListener('mouseup',e=>{
@@ -77,14 +84,20 @@ export class Colorpicker extends React.Component{
     }
 
     generateFromClick = e => {
+        /*判断event是否在范围内 */
+        const canvasToTop = this.canvas.getBoundingClientRect().top
+        const canvasToLeft = this.canvas.getBoundingClientRect().left
+        const x = gTarget(e.clientX,canvasToLeft,400),
+        y = gTarget(e.clientY,canvasToTop,this.canvasline.height)
+
         let _bgcolor = this.state.bgColor,
             _bgred = parseInt(_bgcolor.slice(4, -1).split(',')[0]),
             _bggreen = parseInt(_bgcolor.slice(4, -1).split(',')[1]),
             _bgblue = parseInt(_bgcolor.slice(4, -1).split(',')[2]);
         const position = {}
-        position.top = e.clientY - 9 - this.canvas.getBoundingClientRect().top + 'px';
-        position.left = e.clientX - 9 - this.canvas.getBoundingClientRect().left+ 'px';
-        const newColor = calculateColor(e.clientX, e.clientY, _bgred, _bggreen, _bgblue,this.state.color)
+        position.top = y- 9 - this.canvas.getBoundingClientRect().top + 'px';
+        position.left = x - 9 - this.canvas.getBoundingClientRect().left+ 'px';
+        const newColor = calculateColor(x, y, _bgred, _bggreen, _bgblue,this.state.color)
         this.setState({
             color: newColor,
             position,
@@ -93,7 +106,7 @@ export class Colorpicker extends React.Component{
 
     generateColor = (key,value) => {
         const color = this.state.color
-        color[key] = Number(value);
+        color[key] = key ==='hex' ? value : Number(value) 
         let newColor
         switch(key){
             case 'R':
@@ -113,18 +126,25 @@ export class Colorpicker extends React.Component{
         }
         /*根据color计算小圈的位置 */
         const [bgColor,position] = gPositionByColor(newColor)
+        const position2 = {
+            top:calculatePositionOnLine(bgColor) * this.canvasline.height - 9  +'px'
+        }
         this.setState({
             color:newColor,
             bgColor,
             position,
+            position2,
         })
     }
 
     changeBgColor = e => {
+        /*判断event是否在范围内 */
+        const canvaslineToTop = this.canvasline.getBoundingClientRect().top
+        const y = gTarget(e.clientY,canvaslineToTop,this.canvasline.height)
         const { color,position } = this.state
         const position2 = {}
-        let c = this.context2.getImageData(10, e.clientY, 1, 1).data;
-        position2.top = e.clientY - 9 -this.canvasline.getBoundingClientRect().top + 'px';
+        let c = this.context2.getImageData(5, y-canvaslineToTop-0.5, 1, 1).data;
+        position2.top = y - 9 -canvaslineToTop + 'px';
         const bgColor = 'rgb(' + c[0] + ',' + c[1] + ',' + c[2] + ')';
         const newColor = calculateColor(parseInt(position.left)+9, parseInt(position.top)+9, c[0],c[1],c[2],color)
         this.setState({
@@ -135,6 +155,7 @@ export class Colorpicker extends React.Component{
     }
 
     render(){
+        const { mode, hsl, rgb, hex, visible } = this.config
         // 这里 state 的 color 改变后，需要重新 render 两个canvas 所有的input
         const inputProps = (key) => ({
             labelText:key,
@@ -142,29 +163,45 @@ export class Colorpicker extends React.Component{
             onChange:e => this.generateColor(key,e.target.value)
         })
 
+        const circlestyle = {
+            ...this.state.position,
+            backgroundColor: this.state.color.hex,
+            height: '18px',
+            width: '18px'
+        }
+
+        const circlestyle2 = {
+            ...this.state.position2,
+            backgroundColor: this.state.bgColor,
+            height: '18px',
+            width: '18px'
+        }
+
+
         return (
-            <div className="rcp-container" style={{visibility:this.state.visible}}>
+            <div className={`rcp-container-${mode}`} style={{visibility:this.state.visible}}>
                 <div className='rcp-canvas rcp-canvas-pr' ref={v =>this.canvas = v} onClick={this.generateFromClick} style={{backgroundColor:this.state.bgColor}}>
-                    <div className='circle circle1' style={this.state.position}></div>
+                    <div className='circle circle1' style={circlestyle}></div>
                     <div className='rcp-canvas ver'></div>
                     <div className='rcp-canvas hiz'></div>
                 </div>
                 <div className="rcp-canvas-pr rcp-canvasline-container" ref={v =>this.canvaslinecontainer = v} >
-                    <canvas ref={v =>this.canvasline = v} onClick={this.changeBgColor} ></canvas>
-                    <div className='circle circle2' style={this.state.position2}></div>
+                    <canvas className="rcp-canvasline" ref={v =>this.canvasline = v} onClick={this.changeBgColor} ></canvas>
+                    <div className='circle circle2' style={circlestyle2}></div>
                 </div>
-                <div className='rcp-control-base'>
-                    <div className='rcp-preview' style={{backgroundColor:this.state.color.hex}}></div>
-                    <div className='rcp-control-section'>
-                        <ColorInput {...inputProps('R')}/>
-                        <ColorInput {...inputProps('G')}/>
-                        <ColorInput {...inputProps('B')}/>
-                    </div>
-                    <div className='rcp-control-section'>
+                <div className='rcp-control-base' style={{backgroundColor:this.state.color.hex}}>
+                    {rgb && <div className='rcp-control-section'>
+                        <label>rgb:</label>
+                        <ColorInput {...inputProps('R')}/>,<ColorInput {...inputProps('G')}/>,<ColorInput {...inputProps('B')}/>
+                    </div>}
+                    {hsl && <div className='rcp-control-section'>
                         <ColorInput {...inputProps('H')} step={0.01}/>
                         <ColorInput {...inputProps('S')} step={0.01}/>
                         <ColorInput {...inputProps('L')} step={0.01}/>
-                    </div>
+                    </div>}
+                    {hex && <div className='rcp-control-section'>
+                        <ColorInput {...inputProps('hex')} type="text"/>
+                    </div>}
                 </div>
             </div>
         )
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,15 @@
-const RGBToHSL = (r, g, b) =>{
+const RGBToHSL = function RGBToHSL(r, g, b) {
     r /= 255, g /= 255, b /= 255;
-    let max = Math.max(r, g, b),
+    const max = Math.max(r, g, b),
         min = Math.min(r, g, b);
-    let h, s, l = (max + min) / 2;
+    let h = void 0,
+        s = void 0,
+        l = (max + min) / 2;
 
     if (max == min) {
         h = s = 0; // achromatic
     } else {
-        let d = max - min;
+        const d = max - min;
         s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
         switch (max) {
             case r:
@@ -22,36 +24,154 @@ const RGBToHSL = (r, g, b) =>{
         }
         h /= 6;
     }
-    this._inputRGB[3].value = h.toFixed(2);
-    this._inputRGB[4].value = s.toFixed(2);
-    this._inputRGB[5].value = l.toFixed(2);
-}
+    return [Number(h.toFixed(2)), Number(s.toFixed(2)), Number(l.toFixed(2))]
+};
 
-const HSLToRGB = (h, s, l) => {
+const HSLToRGB = function HSLToRGB(h, s, l) {
     h = parseFloat(h);
     s = parseFloat(s);
     l = parseFloat(l);
-    let _r, _g, _b;
+    let _r = void 0,
+        _g = void 0,
+        _b = void 0;
     if (s == 0) {
         _r = _g = _b = l; // achromatic
     } else {
-        function hue2rgb(p, q, t) {
+        const hue2rgb = function hue2rgb(p, q, t) {
             if (t < 0) t += 1;
             if (t > 1) t -= 1;
             if (t < 1 / 6) return p + (q - p) * 6 * t;
             if (t < 1 / 2) return q;
             if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
             return p;
-        }
-        let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        let p = 2 * l - q;
+        };
+
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        const p = 2 * l - q;
         _r = hue2rgb(p, q, h + 1 / 3);
         _g = hue2rgb(p, q, h);
         _b = hue2rgb(p, q, h - 1 / 3);
     }
-    this._inputRGB[0].value = Math.round(_r * 255);
-    this._inputRGB[1].value = Math.round(_g * 255);
-    this._inputRGB[2].value = Math.round(_b * 255);
+    return [Math.round(_r * 255), Math.round(_g * 255), Math.round(_b * 255)]
+};
+
+const calculateColor = (x, y, r, g, b, color) => {
+    color.R = calculateRGB(r, x, y);
+    color.G = calculateRGB(g, x, y);
+    color.B = calculateRGB(b, x, y);
+    return generateColor('rgb', color)
 }
 
-export { HSLToRGB, RGBToHSL }
+const calculateRGB = (num, x, y) => {
+    return Math.round((255 - (255 - num) * (x / 400)) * (1 - y / 400))
+}
+
+const componentToHex = c => {
+    const hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+const rgbToHex = (r, g, b) => {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+const hexToRgb = hex => {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+    ] : null;
+}
+
+const generateColor = (type, color) => {
+    switch (type) {
+        case 'rgb':
+            [color.H, color.S, color.L] = RGBToHSL(color.R, color.G, color.B)
+            color.hex = rgbToHex(color.R, color.G, color.B)
+            break;
+        case 'hsl':
+            [color.R, color.G, color.B] = HSLToRGB(color.H, color.S, color.L)
+            color.hex = rgbToHex(color.R, color.G, color.B)
+            break;
+        case 'hex':
+            const [R, G, B] = hexToRgb(color.hex);
+            [color.H, color.S, color.L] = RGBToHSL(R, G, B);
+            [color.R, color.G, color.B] = [R, G, B];
+            console.log(color)
+        break;
+
+    }
+    return color
+}
+
+
+const gPositionByColor = (color) => {
+    let R = {
+            name: 'r',
+            value: color.R,
+            calculate: null
+        },
+        G = {
+            name: 'g',
+            value: color.G,
+            calculate: null
+        },
+        B = {
+            name: 'b',
+            value: color.B,
+            calculate: null
+        },
+        _array = [R, G, B];
+    _array.sort(function (b, a) {
+        return a.value - b.value
+    })
+    let bgColor, position = {}
+    if (_array[0].value == _array[1].value) {
+        _array[0].calculate = 255;
+        _array[1].calculate = 255;
+        _array[2].calculate = 0;
+        bgColor = 'rgb(' + R.calculate + ',' + G.calculate + ',' + B.calculate + ')';
+        let y = 400 * (1 - _array[0].value / 255),
+            x = 400 * (1 - _array[2].value / _array[0].value);
+        position.top = y - 9 + 'px';
+        position.left = x - 9 + 'px';
+
+    } else {
+        _array[0].calculate = 255;
+        _array[2].calculate = 0;
+        let y = 400 * (1 - _array[0].value / 255),
+            x = 400 * (1 - _array[2].value / _array[0].value);
+        _array[1].calculate = 255 - (400 / x) * (255 - 400 * _array[1].value / (400 - y))
+        bgColor = 'rgb(' + Math.round(R.calculate) + ',' + Math.round(G.calculate) + ',' + Math.round(B.calculate) + ')';
+        position.top = y - 9 + 'px';
+        position.left = x - 9 + 'px';
+    }
+
+    return [bgColor, position]
+}
+
+const calculatePositionOnLine = (bgColor) => {
+    const _bgred = parseInt(bgColor.slice(4, -1).split(',')[0]),
+        _bggreen = parseInt(bgColor.slice(4, -1).split(',')[1]),
+        _bgblue = parseInt(bgColor.slice(4, -1).split(',')[2]);
+    const [H, S, L] = RGBToHSL(_bgred, _bggreen, _bgblue)
+    return H
+}
+
+const gTarget = (target,min,length) => {
+    if(target < min){
+        return min
+    } else if(target > min + length){
+        return min + length
+    }
+    return target
+}
+
+export {
+    calculateColor,
+    generateColor,
+    gPositionByColor,
+    calculatePositionOnLine,
+    gTarget
+};
